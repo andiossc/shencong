24ab14d4738b9d1d350199104badd090513d96e6
andiossc <1491161426@qq.com>
2017-04-05 20:55:38 +0800

    第二次提交
    修改了侧边栏的逻辑实现,整个页面的遮罩

--- js/main.js
+++ js/main.js
@@ -1,39 +1,88 @@
 /**
  * Created by Administrator on 2017/4/1.
  */
-window.onload = function () {
-    var menuIcon = document.getElementById('menuIcon');
-    var menuContent = document.getElementById("menuContent");
-    var w = document.documentElement.clientWidth ||
-                document.body.clientWidth;
-    var menuClose = document.getElementById("menuClose");
-    var isOpen = false;
+//window.onload = function () {
+//    var menuIcon = findEle('menuIcon');
+//    var menuContent = findEle("menuContent");
+//    var w = document.documentElement.clientWidth ||
+//                document.body.clientWidth;
+//    var menuClose = findEle("menuClose");
+//    var isOpen = false;
+//    var button = findEle('button');
+//
+//    menuIcon.onclick = function () {
+//        if(isOpen) {
+//            startMove(w);
+//        }else{
+//            startMove(w - 350);
+//        }
+//        isOpen = !isOpen;
+//    }
+//
+//    menuClose.onclick = function(){
+//        startMove(w);
+//        isOpen = false;
+//    };
+//    button.onclick = function () {
+//        startMove(w-350);
+//        isOpen = true;
+//    }
+//};
+//
+///*根据ID返回节点*/
+//function findEle(id) {
+//    return document.getElementById(id);
+//}
+//
+//
+//var timer;
+//function startMove(iTarget) {
+//    clearInterval(timer);
+//    var menuContent = document.getElementById('menuContent');
+//    timer = setInterval(function () {
+//        var speed = (iTarget - menuContent.offsetLeft) / 10;
+//        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+//        if (menuContent.offsetLeft === iTarget) {
+//            clearInterval(timer);
+//        } else {
+//            menuContent.style.left = menuContent.offsetLeft + speed + 'px';
+//        }
+//    }, 30);
+//}
 
-    console.log(menuIcon);
-    menuIcon.onclick = function () {
-        if(isOpen) {
-            startMove(w);
-        }else{
-            startMove(w - 350);
-        }
-        isOpen = !isOpen;
+;
+$(function () {
+    'use strict'
+    var sidebar = $('#sidebar'),
+        mask = $('.mask'),
+        sidebar_trigger = $('#menuIcon'),
+        backButton = $('.back-to-top');
+
+    function showSideBar() {
+        mask.fadeIn();
+//        sidebar.animate({'right':0});
+        sidebar.css('right', 0);
     }
-    menuClose.onclick = function(){
-        startMove(w);
-        isOpen = false;
+
+    function hideSideBar() {
+        mask.fadeOut();
+        sidebar.css('right', -sidebar.width());
     }
-};
-var timer;
-function startMove(iTarget) {
-    clearInterval(timer);
-    var menuContent = document.getElementById('menuContent');
-    timer = setInterval(function () {
-        var speed = (iTarget - menuContent.offsetLeft) / 10;
-        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-        if (menuContent.offsetLeft === iTarget) {
-            clearInterval(timer);
-        } else {
-            menuContent.style.left = menuContent.offsetLeft + speed + 'px';
-        }
-    }, 30);
-}
+
+    sidebar_trigger.on('click', showSideBar);
+    mask.on('click', hideSideBar);
+    backButton.on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 800)
+    });
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > $(window).height())
+            backButton.fadeIn();
+        else
+            backButton.fadeOut();
+    })
+    $(window).trigger('scroll');
+})
+
+
