877de533732ed23ca78b9a394f2abc7a4a3d254e
shencong552@qq.com <shencong552@qq.com>
2019-09-01 15:51:32 +0800

    生日页

---
+++ js/login.js
@@ -0,0 +1,37 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="小可爱"){
+    	if(pwd == "520") {
+            event.preventDefault();
+            $('form').fadeOut(500);
+            $('.wrapper').addClass('form-success');
+            requestFullScreen();
+            setTimeout(function () {
+                location.href = "BirthdayCake.html";
+            }, 2000);
+        }else{
+            alert("难道猜不出密码吗(•́へ•́╬)");
+		}
+    }else{
+		alert("我对你的爱称就是用户名ヾ(*Ő౪Ő*)");
+	}
+});
+
+function requestFullScreen(element) {
+	var element=document.documentElement;
+	var requestMethod = element.requestFullScreen || //W3C
+	element.webkitRequestFullScreen || //Chrome等
+	element.mozRequestFullScreen || //FireFox
+	element.msRequestFullScreen; //IE11
+	if (requestMethod) {
+		requestMethod.call(element);
+	}
+	else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
+		var wscript = new ActiveXObject("WScript.Shell");
+		if (wscript !== null) {
+		 wscript.SendKeys("{F11}");
+		}
+	}
+}
